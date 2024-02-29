c053876ec7bb0dc3356e420afa4d41c299097ba0
Distillery <Distillery@DESKTOP-9N56E1G>
2024-02-29 13:50:16 -0300

    Add project files.

---
+++ ThePretendCompanyApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TCPData;
+using TCPExtensions;
+
+namespace ThePretendCompanyApplication // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeList = Data.GetEmployees();
+
+            var filteredEmployees = employeeList.Filter(emp => emp.AnnualSalary < 50000);
+
+            foreach (var employee in filteredEmployees)
+            {
+                Console.WriteLine($"First Name: {employee.FirstName}");
+                Console.WriteLine($"Last Name: {employee.LastName}");
+                Console.WriteLine($"Annual salary: {employee.AnnualSalary}");
+                Console.WriteLine($"Manager: {employee.IsManager}");
+                Console.WriteLine($" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
