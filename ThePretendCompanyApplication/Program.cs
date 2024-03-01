79bf67f95b20d3e28d56fbe9f8cc184303a7f038
fercanepari <fercanepari@gmail.com>
2024-03-01 10:17:44 -0300

    Linq added

--- ThePretendCompanyApplication/Program.cs
+++ ThePretendCompanyApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using TCPData;
 using TCPExtensions;
+using System.Linq;
 
 namespace ThePretendCompanyApplication // Note: actual namespace depends on the project name.
 {
@@ -9,15 +10,42 @@ namespace ThePretendCompanyApplication // Note: actual namespace depends on the 
         static void Main(string[] args)
         {
             List<Employee> employeeList = Data.GetEmployees();
+            List<Department> departmentList = Data.GetDepartments();
 
-            var filteredEmployees = employeeList.Filter(emp => emp.AnnualSalary < 50000);
+            //Extension method and lambda function to filter
+            //var filteredEmployees = employeeList.Filter(emp => emp.AnnualSalary < 50000);
 
-            foreach (var employee in filteredEmployees)
+            //After importing System.Linq We can use the 'Where' extended method
+            var filteredEmployees = employeeList.Where(emp => emp.IsManager == true);
+
+            //foreach (var employee in filteredEmployees)
+            //{
+            //Console.WriteLine($"First Name: {employee.FirstName}");
+            //Console.WriteLine($"Last Name: {employee.LastName}");
+            //Console.WriteLine($"Annual salary: {employee.AnnualSalary}");
+            //Console.WriteLine($"Manager: {employee.IsManager}");
+            //Console.WriteLine($" ");
+            //}
+
+            var resultList = from emp in employeeList
+                             join dept in departmentList
+                             on emp.DepartmentId equals dept.Id
+                             select new
+                             {
+                                 FirstName = emp.FirstName,
+                                 LastName = emp.LastName,
+                                 AnnualSalary = emp.AnnualSalary,
+                                 Manager = emp.IsManager,
+                                 Department = dept.LongName
+                             };
+
+            foreach (var employee in resultList)
             {
                 Console.WriteLine($"First Name: {employee.FirstName}");
                 Console.WriteLine($"Last Name: {employee.LastName}");
                 Console.WriteLine($"Annual salary: {employee.AnnualSalary}");
-                Console.WriteLine($"Manager: {employee.IsManager}");
+                Console.WriteLine($"Manager: {employee.Manager}");
+                Console.WriteLine($"Department: {employee.Department}");
                 Console.WriteLine($" ");
             }
 
